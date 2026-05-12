f5871e1d7e985bba9a2ba3d3e7f34b5fca877aa5
alesalpi79-creator <alesalpi79@gmail.com>
2026-05-12 07:21:07 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1176,8 +1176,8 @@ const VideoOverlay = ({ onClose }) => (
         <iframe 
           width="100%" 
           height="100%" 
-          src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" 
-          title="Shift Pro Demo" 
+          src="https://www.youtube.com/embed/6_6v9eO-2tY?autoplay=1" 
+          title="Turni Pro Demo" 
           frameBorder="0" 
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
           allowFullScreen
